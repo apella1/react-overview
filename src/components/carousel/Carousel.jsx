628fa2e45031fe0d2ff9911a69e7b0fe26bd8979
apella1 <johncollons5@gmail.com>
2023-04-24 16:15:36 +0300

    Add carousel data

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -51,8 +51,8 @@ const Carousel = () => {
 
   return (
     <div className="carousel my-12 mx-auto">
-      <h2 className="text-4xl leading-8 font-semibold mb-12 text-slate-700">
-        Our epic carousel
+      <h2 className="text-4xl leading-8 text-center font-semibold mb-12 text-black">
+    Carousel.
       </h2>
       <div className="relative overflow-hidden">
         <div className="flex justify-between absolute top left w-full h-full">
